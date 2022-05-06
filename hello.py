c5af7136c0b456c584538bf32faeeed7c9bd478b
y001003 <y001003@naver.com>
2022-05-06 15:17:32 +0900

    feat: %5 ==0

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
-for i in range(1,21):
-	if i% 15 == 0:
-		print('yeardream')
-	elif i % 3 == 0:
+for i in range(1,25+1):
+	if i%3==0:
 		print('year')
-	else:
+	elif i%5==0:
+		print('dream')
+	else :
 		print(i)
-
